29a2634d4ad999752f1ce71a8bfff0a027014aea
Michitaka Inoue <michitaka@Michitakas-MacBook-Pro.local>
2015-10-24 22:04:16 -0400

    fixed type

--- module/template.py
+++ module/template.py
@@ -3,13 +3,13 @@
 # class name must be 'Commands'
 class Commands(object):
 
-    #Name of dictionary must be this module name + 'Dict'
-    templeteDict = {}
+    # Name of dictionary must be this module name + 'Dict'
+    templateDict = {}
 
     def __init__(self):
         pass
 
     def _sampleCommand(self):
         print "Hello World"
-    templeteDict['sampleCommand'] = "sphere.png"
+    templateDict['sampleCommand'] = "sphere.png"
     # ^ Don't forget to add the command to the dictionary.
