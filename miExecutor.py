d761e537f8a2cca4f4aa20cd87a121dd2bc52ad1
minoue <michitaka.inoue@icloud.com>
2015-11-02 18:17:40 -0500

    reimpremented QLineEdit's focusOutEvent so that the tool closes when gets focus out.

--- miExecutor.py
+++ miExecutor.py
@@ -92,6 +92,11 @@ class CustomQLineEdit(QtGui.QLineEdit):
 
     def __init__(self, parent=None):
         super(CustomQLineEdit, self).__init__(parent)
+        self.setFocusPolicy(QtCore.Qt.StrongFocus)
+
+    def focusOutEvent(self, event):
+        # emit signal to close the window when it gets focus out
+        self.escPressed.emit('esc')
 
     def keyPressEvent(self, event):
         if event.key() == QtCore.Qt.Key_Escape:
