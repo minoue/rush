aff0eba2c0f7f5a0c9bebbfc9402f04c2c9d6d11
Michitaka Inoue <michitaka@Michitakas-MacBook-Pro.local>
2016-01-07 12:08:03 -0500

    Load user pref file if exists in the maya user script directory

--- preference/miExecPref.py
+++ preference/miExecPref.py
@@ -1,14 +1,23 @@
 import os
 import json
-
+import maya.cmds as cmds
 
 SCRIPT_PATH = os.path.dirname(__file__)
+MAYA_SCRIPT_DIR = cmds.internalVar(userScriptDir=True)
 
 
 def getPreference():
     """ Load pref json data nad return as dict"""
 
-    prefFile = open(os.path.join(SCRIPT_PATH, "miExecPref.json"), 'r')
+    for root, dirs, files in os.walk(MAYA_SCRIPT_DIR):
+        if 'miExecPref.json' in files:
+            # Load pref json file from user script dir if exists.
+            abspath = os.path.join(root, 'miExecPref.json')
+            prefFile = open(abspath, 'r')
+        else:
+            # Load pref json file from miExec package directory.
+            prefFile = open(os.path.join(SCRIPT_PATH, "miExecPref.json"), 'r')
+
     prefDict = json.load(prefFile)
     prefFile.close()
 
