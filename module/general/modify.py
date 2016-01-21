5d1bf1f0bece80e976fd573936fce2c63f500a8f
U-DESKTOP-L43FN13\michi <michitaka.inoue@icloud.com>
2016-01-21 07:52:49 -0500

    Add commands to modify module

--- module/general/modify.py
+++ module/general/modify.py
@@ -1,4 +1,5 @@
 import maya.cmds as cmds
+import maya.mel as mel
 
 
 class Commands(object):
@@ -20,3 +21,43 @@ class Commands(object):
     def _freezeOnlyScale(self):
         cmds.makeIdentity(apply=True, t=False, r=False, s=True, n=False)
     commandDict['freezeOnlyScale'] = "menuIconModify.png"
+
+    def _nurbsToPolygonOptions(self):
+        cmds.NURBSToPolygonsOptions()
+    commandDict['nurbsToPolygonOptions'] = "nurbsToPolygons.png"
+
+    def _nurbsToSubdivOptions(self):
+        cmds.CreateSubdivSurfaceOptions()
+    commandDict['nurbsToSubdivOptions'] = "nurbsToSubdivs.png"
+
+    def _polygonToSubdivOptions(self):
+        cmds.CreateSubdivSurfaceOptions()
+    commandDict['polygonToSubdivOptions'] = "subdivCreate.png"
+
+    def _polygonEdgesToCurveOptions(self):
+        cmds.CreateCurveFromPolyOptions()
+    commandDict['polygonEdgesToCurveOptions'] = "menuIconModify.png"
+
+    def _subdivToPolygonsOptions(self):
+        cmds.TesselateSubdivSurfaceOptions()
+    commandDict['subdivToPolygonsOptions'] = "subdivTessellate.png"
+
+    def _subdivToNURBSOptions(self):
+        cmds.SubdivToNURBSOptions()
+    commandDict['subdivToNURBSOptions'] = "subdivToNurbs.png"
+
+    def _paintEffectsToPolygonsOptions(self):
+        cmds.PaintEffectsToPolyOptions()
+    commandDict['paintEffectsToPolygonsOptions'] = "paintFXtoPoly.png"
+
+    def _paintEffectsToNURBSOptions(self):
+        cmds.PaintEffectsToNurbsOptions()
+    commandDict['paintEffectsToNURBSOptions'] = "paintFXtoNurbs.png"
+
+    def _paintEffectsToCurveOptions(self):
+        cmds.PaintEffectsToCurveOptions()
+    commandDict['paintEffectsToCurveOptions'] = "paintFXtoCurve.png"
+
+    def _textureToGeometryOptions(self):
+        mel.eval("""performTextureToGeom 1""")
+    commandDict['textureToGeometryOptions'] = "textureToGeom.png"
