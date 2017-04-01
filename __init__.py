a411f7caca756caa1bf5c01da9c1c3427cb6462e
minoue <michitaka.inoue@icloud.com>
2017-04-01 20:31:01 +0100

    New init file

--- __init__.py
+++ __init__.py
@@ -1,26 +1,88 @@
-import Qt
-import app
-from app.utils import generalUtils
-reload(app)
+from maya import cmds
+import json
+import imp
+import os
 
 
-@generalUtils.timer
-def main():
-    """ Show window and move it to cursor position """
+def getClassList():
+    """
 
-    # Create and show window
-    global miExec
-    try:
-        miExec.close()
-    except:
-        pass
+    Args:
+        param (logger): logger
 
-    miExec = app.MainWindow()
-    miExec.show()
+    Return:
+        list: list of classes
 
-    # Move the window to the cursor position.
-    pos = Qt.QtGui.QCursor.pos()
-    miExec.move(
-        pos.x() - (miExec.width() / 2), pos.y() - (miExec.height() / 2))
-    miExec.activateWindow()
-    miExec.raise_()
+    """
+
+    moduleDirName = "rush"
+    mayaScriptDir = cmds.internalVar(userScriptDir=True)
+
+    moduleRoot = os.path.join(mayaScriptDir, moduleDirName)
+
+    moduleList = []
+    for root, dirs, files in os.walk(moduleRoot):
+        for f in files:
+            fullPath = os.path.join(root, f)
+            if (fullPath.endswith(".py") and not
+                    fullPath.endswith("__init__.py")):
+                moduleList.append(fullPath)
+
+    moduleObjectList = []
+
+    for path in moduleList:
+        # Create module names for import, for exapmle ...
+        #
+        # "rush/template"
+        # "animation/animate"
+        # "common/create"
+        # "common/display"
+
+        name = os.path.splitext(path)[0].split("/")
+        name = "/".join(name[-2:])
+
+        try:
+            mod = imp.load_source(name, path)
+            moduleObjectList.append(mod)
+        except ImportError:
+            pass
+
+    commandClassList = [i.Commands for i in moduleObjectList]
+
+    cmdsDict = {}
+    for c in commandClassList:
+        cmdsDict.update(c.commandDict)
+
+    saveCommands(cmdsDict)
+
+    return commandClassList
+
+
+def saveCommands(cmdsDict):
+    """ Save all commands as a json file in the maya user directory
+    
+    Args:
+        cmdsDict (dict): All commands
+    
+    Return:
+        None
+    
+    """
+    outPath = os.path.normpath(os.path.join(mayaScriptDir, "rushCmds.json"))
+
+    with open(outPath, 'w') as outFile:
+        json.dump(
+            cmdsDict,
+            outFile,
+            indent=4,
+            separators=(',', ':'),
+            sort_keys=True)
+
+
+class RushCommands():
+    pass
+
+
+# Re-difine RushCommands class to inherit all comamnd classes for the list
+cl = tuple(getClassList())
+RushCommands = type('RushCommands', cl, dict(RushCommands.__dict__))
