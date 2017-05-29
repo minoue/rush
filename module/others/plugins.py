aa6837e14e520f5917cf1c452bd0c9a8ce2a27dd
minoue <michitaka.inoue@icloud.com>
2017-05-29 23:24:40 +0100

    Add new module for plugin loading

---
+++ module/others/plugins.py
@@ -0,0 +1,13 @@
+from maya import cmds
+
+
+class Commands(object):
+    """ class name must be 'Commands' """
+
+    commandDict = {}
+
+    def _loadObjPlugin(self):
+        if not cmds.pluginInfo("objExport", q=True, loaded=True):
+            cmds.loadPlugin("objExport")
+    commandDict['sampleCommand'] = "sphere.png"
+    # ^ Don't forget to add the command to the dictionary.
